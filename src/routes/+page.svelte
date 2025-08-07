e7c94dce58be593420096a71c93e898b119e5bfd
trunov <k.trunov.k@yandex.ru>
2025-08-07 14:42:17 +0300

    add initial scaffold for svelte project

---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+	import './+page.css';
+</script>
+
+<div class="hero">
+	<header class="header">
+		<p class="logo">Secret Chat</p>
+	</header>
+
+	<section class="content">
+		<h1>messenger</h1>
+		<p>will launch soon</p>
+	</section>
+</div>
